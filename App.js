66097cefb475d7958dbfbedf98d1f6cc287b8416
Carlos Velasquez <imadogsi64@gmail.com>
2019-03-11 11:29:19 -0600

    Map

--- App.js
+++ App.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MapView, StatusBar } from 'expo';
 
 export default class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={streng:""}
+  }
   render() {
+
+    fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({streng:JSON.stringify(responseJson.movies)});
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <View style={{flex:1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 14.0481,
+          longitude: -87.1741,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        ><MapView.Marker coordinate={{
+          latitude: 14.0481,
+          longitude: -87.1741,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        pinColor="blue"
+        title="unitek"
+        description="untik"/></MapView>
+        <View>
+          <Text>{this.state.streng}</Text>
+        </View >
+        </View>
+
     );
   }
 }
