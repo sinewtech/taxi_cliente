d57eb0986b26971755b29fafcfbfde44cd4841ab
Josue David <josue.rodriguez1551@gmail,com>
2019-03-13 08:57:24 -0600

    Desarrollo del searchbar

--- App.js
+++ App.js
@@ -1,47 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { MapView, StatusBar } from 'expo';
+import { StyleSheet, Text, View, StatusBar, TextInput, Alert, Button } from 'react-native';
+import { MapView } from 'expo';
 
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.state={streng:""}
+    this.state = { streng: "", busqueda: "" }
   }
-  render() {
 
+  render() {
     fetch('https://facebook.github.io/react-native/movies.json')
       .then((response) => response.json())
       .then((responseJson) => {
-        this.setState({streng:JSON.stringify(responseJson.movies)});
+        this.setState({ streng: JSON.stringify(responseJson.movies) });
       })
       .catch((error) => {
         console.error(error);
       });
 
     return (
-      <View style={{flex:1 }}>
-      <MapView
-        style={{ flex: 1 }}
-        initialRegion={{
-          latitude: 14.0481,
-          longitude: -87.1741,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-        ><MapView.Marker coordinate={{
-          latitude: 14.0481,
-          longitude: -87.1741,
-          latitudeDelta: 0.01,
-          longitudeDelta: 0.01,
-        }}
-        pinColor="blue"
-        title="unitek"
-        description="untik"/></MapView>
-        <View>
-          <Text>{this.state.streng}</Text>
-        </View >
+      <View style={styles.container}>
+        <MapView
+          style={{ flex: 1 }}
+          showsCompass={false}
+          initialRegion={{
+            latitude: 14.0481,
+            longitude: -87.1741,
+            latitudeDelta: 0.01,
+            longitudeDelta: 0.01,
+          }}/>
+        <View style={{position:"absolute", flex: 1, width:"100%"}}>
+          < TextInput style={styles.searchBar} placeholder={"Buscar un lugar"}
+            onChangeText={(busqueda) => this.setState({ busqueda })} />
         </View>
-
+      </View>
     );
   }
 }
@@ -50,7 +42,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  },
+
+  searchBar: {
+    fontSize: 18,
+    borderRadius: 10,
+    marginTop: 35,
+    marginRight: 15,
+    marginLeft: 15,
+    backgroundColor: "#ffffff",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#cceeff",
+    paddingLeft: 10,
   },
 });
