b1678fa33211165dff9328b2d687d9144448c022
David Mendoza <D.mendoza99@unitec.edu>
2019-05-26 23:42:42 -0600

    cambios de functions

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const fetch = require("node-fetch");
 admin.initializeApp();
 const location = express();
 var bodyParser = require("body-parser");
