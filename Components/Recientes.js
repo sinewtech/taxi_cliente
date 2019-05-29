025eabaff0766a383a78ca0df39e04da0261e63b
Carlos Velasquez <imadogsi64@gmail.com>
2019-05-28 18:25:03 -0600

    Sin Olo

--- Components/Recientes.js
+++ Components/Recientes.js
@@ -2,19 +2,23 @@ import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
 export default class Recientes extends React.Component {
-    render(){
-        return(
-            <View style={styles.recientesView}>
-                <Text>
-                    Olo
-                </Text>
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={[styles.recientesView, styles.fullCenter]}>
+        <Text>Comienza a escribir para ver lugares</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    recientesView:{
-        color: "gray"
-    }
-})
+  recientesView: {
+    color: "gray",
+  },
+
+  fullCenter: {
+    margin: "auto",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
