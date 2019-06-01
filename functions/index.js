9232bff1175da95431f455354b4753f9bb1d1604
Carlos Velasquez <imadogsi64@gmail.com>
2019-06-01 12:44:57 -0600

    switch en el massive_notifications

--- functions/index.js
+++ functions/index.js
@@ -193,114 +193,110 @@ exports.confirm_quote = functions.database.ref("quotes/{uid}").onUpdate(snapshot
   }
 });
 
-exports.find_reference = functions.database
-  .ref("quotes/{uid}")
-  .onCreate((snapshot, context) => {
-    let data = snapshot.exportVal();
+exports.find_reference = functions.database.ref("quotes/{uid}").onCreate((snapshot, context) => {
+  let data = snapshot.exportVal();
 
-    if (data.usingGps && !data.manual) {
-      let query =
-        "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" +
-        API_KEY +
-        "&location=" +
-        data.origin.lat +
-        "," +
-        data.origin.lng +
-        "&radius=" +
-        REFERENCE_RADIUS;
+  if (data.usingGps && !data.manual) {
+    let query =
+      "https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=" +
+      API_KEY +
+      "&location=" +
+      data.origin.lat +
+      "," +
+      data.origin.lng +
+      "&radius=" +
+      REFERENCE_RADIUS;
 
-      fetch(query)
-        .then(response => response.json())
-        .then(responseJson => {
-          let foundPlace = false;
+    fetch(query)
+      .then(response => response.json())
+      .then(responseJson => {
+        let foundPlace = false;
 
-          for (let place of responseJson.results) {
-            if (place.name != "Tegucigalpa") {
-              console.log("Se encontró un lugar cercano:", place.name);
-              foundPlace = true;
-              console.log("UID", context.params.uid);
+        for (let place of responseJson.results) {
+          if (place.name != "Tegucigalpa") {
+            console.log("Se encontró un lugar cercano:", place.name);
+            foundPlace = true;
+            console.log("UID", context.params.uid);
 
-              let detailsQuery =
-                "https://maps.googleapis.com/maps/api/place/details/json?key=" +
-                API_KEY +
-                "&placeid=" +
-                place.place_id;
+            let detailsQuery =
+              "https://maps.googleapis.com/maps/api/place/details/json?key=" +
+              API_KEY +
+              "&placeid=" +
+              place.place_id;
 
-              fetch(detailsQuery)
-                .then(detailsResponse => detailsResponse.json())
-                .then(detailsResponseJson => {
-                  var updates = {};
-                  updates["/quotes/" + context.params.uid + "/origin/name"] =
-                    "Cerca de " + place.name;
-                  updates["/quotes/" + context.params.uid + "/origin/address"] =
-                    detailsResponseJson.result.formatted_address;
+            fetch(detailsQuery)
+              .then(detailsResponse => detailsResponse.json())
+              .then(detailsResponseJson => {
+                var updates = {};
+                updates["/quotes/" + context.params.uid + "/origin/name"] =
+                  "Cerca de " + place.name;
+                updates["/quotes/" + context.params.uid + "/origin/address"] =
+                  detailsResponseJson.result.formatted_address;
 
-                  admin
-                    .database()
-                    .ref()
-                    .update(updates);
-                })
-                .catch(e => {
-                  console.error(e);
-                  return false;
-                });
+                admin
+                  .database()
+                  .ref()
+                  .update(updates);
+              })
+              .catch(e => {
+                console.error(e);
+                return false;
+              });
 
-              break;
-            }
+            break;
           }
+        }
 
-          if (!foundPlace) {
-            console.log("Lugar cercano no encontrado.");
-            var updates = {};
-            updates["/quotes/" + context.params.uid + "/origin/name"] = "Ubicación Exacta";
-            updates["/quotes/" + context.params.uid + "/origin/address"] =
-              "Lat: " + data.origin.lat + " | Lon: " + data.origin.lng;
-            admin
-              .database()
-              .ref()
-              .update(updates);
-          }
+        if (!foundPlace) {
+          console.log("Lugar cercano no encontrado.");
+          var updates = {};
+          updates["/quotes/" + context.params.uid + "/origin/name"] = "Ubicación Exacta";
+          updates["/quotes/" + context.params.uid + "/origin/address"] =
+            "Lat: " + data.origin.lat + " | Lon: " + data.origin.lng;
+          admin
+            .database()
+            .ref()
+            .update(updates);
+        }
+      })
+      .catch(e => {
+        console.error(e);
+        return false;
+      });
+  }
+});
+
+exports.operator_notification = functions.database.ref("quotes/{uid}").onCreate(snapshot => {
+  let data = snapshot.exportVal();
+
+  admin
+    .database()
+    .ref()
+    .child("Notification_Data/")
+    .once("value", snap => {
+      var message = {
+        data: {
+          title: "Nueva orden recibida",
+          body: "De " + data.origin.name + " a " + data.destination.name,
+        },
+        token: snap.exportVal().token,
+      };
+      return admin
+        .messaging()
+        .send(message)
+        .then(response => {
+          // Response is a message ID string.
+          console.log("Successfully sent message:", response);
+          return false;
         })
-        .catch(e => {
-          console.error(e);
+        .catch(error => {
+          console.log("Error sending message:", error);
           return false;
         });
-    }
-  });
+    });
 
-exports.operator_notification = functions.database
-  .ref("quotes/{uid}")
-  .onCreate((snapshot) => {
-    let data = snapshot.exportVal();
-
-    admin
-      .database()
-      .ref()
-      .child("Notification_Data/")
-      .once("value", snap => {
-        var message = {
-          data: {
-            title: "Nueva orden recibida",
-            body: "De " + data.origin.name + " a " + data.destination.name,
-          },
-          token: snap.exportVal().token,
-        };
-        return admin
-          .messaging()
-          .send(message)
-          .then(response => {
-            // Response is a message ID string.
-            console.log("Successfully sent message:", response);
-            return false;
-          })
-          .catch(error => {
-            console.log("Error sending message:", error);
-            return false;
-          });
-      });
-
-    return true;
-  });
+  return true;
+});
 
 exports.download_url_generator = functions.storage.object().onFinalize(object => {
   const contentType = object.contentType;
@@ -336,18 +332,18 @@ exports.download_url_generator = functions.storage.object().onFinalize(object =>
   });
 });
 
-exports.massive_notifications = functions.firestore
-  .document("Ads/{uid}")
-  .onCreate(snapshot => {
-    let data = snapshot.data();
+exports.massive_notifications = functions.firestore.document("Ads/{uid}").onCreate(snapshot => {
+  let data = snapshot.data();
 
-    if (data.who === "0") {
+  switch (data.who) {
+    case "0":
       return admin
         .firestore()
         .collection("clients")
         .get()
         .then(querySnapshot => {
           let messages = [];
+
           querySnapshot.forEach(doc => {
             let token = doc.data()["pushDevices"][0];
             messages.push({
@@ -362,6 +358,9 @@ exports.massive_notifications = functions.firestore
               },
             });
           });
+
+          console.log("Enviando mensajes a clientes: ", messages);
+
           fetch("https://exp.host/--/api/v2/push/send", {
             method: "POST",
             mode: "no-cors",
@@ -373,7 +372,7 @@ exports.massive_notifications = functions.firestore
             body: JSON.stringify(messages),
           });
         });
-    } else if (data.who === "1") {
+    case "1":
       return admin
         .firestore()
         .collection("drivers")
@@ -394,7 +393,9 @@ exports.massive_notifications = functions.firestore
               },
             });
           });
-          console.log("mensajes", messages);
+
+          console.log("Enviando mensajes a conductores: ", messages);
+
           fetch("https://exp.host/--/api/v2/push/send", {
             method: "POST",
             mode: "no-cors",
@@ -406,69 +407,62 @@ exports.massive_notifications = functions.firestore
             body: JSON.stringify(messages),
           });
         });
-    } else if (data.who === "2") {
-      admin
+    case "2":
+      return admin
         .firestore()
         .collection("clients")
         .get()
         .then(querySnapshot => {
-          let messages = [];
-          querySnapshot.forEach(doc => {
-            let token = doc.data()["pushDevices"][0];
-            messages.push({
-              to: token,
-              sound: "default",
-              title: data.title,
-              body: data.message,
-              channelId: "ads",
-              priority: "high",
-              data: {
-                id: 0,
-              },
+          admin
+            .firestore()
+            .collection("drivers")
+            .get()
+            .then(driverquerySnapshot => {
+              let messages = [];
+
+              querySnapshot.forEach(doc => {
+                let token = doc.data()["pushDevices"][0];
+                messages.push({
+                  to: token,
+                  sound: "default",
+                  title: data.title,
+                  body: data.message,
+                  channelId: "ads",
+                  priority: "high",
+                  data: {
+                    id: 0,
+                  },
+                });
+              });
+
+              driverquerySnapshot.forEach(doc => {
+                let token = doc.data()["pushDevices"][0];
+                messages.push({
+                  to: token,
+                  sound: "default",
+                  title: data.title,
+                  body: data.message,
+                  channelId: "ads",
+                  priority: "high",
+                  data: {
+                    id: 0,
+                  },
+                });
+              });
+
+              console.log("Enviando mensajes a clientes y conductores:", messages);
+
+              fetch("https://exp.host/--/api/v2/push/send", {
+                method: "POST",
+                mode: "no-cors",
+                headers: {
+                  Accept: "application/json",
+                  "Content-Type": "application/json",
+                  "Access-Control-Allow-Origin": "*",
+                },
+                body: JSON.stringify(messages),
+              });
             });
-          });
-          fetch("https://exp.host/--/api/v2/push/send", {
-            method: "POST",
-            mode: "no-cors",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*",
-            },
-            body: JSON.stringify(messages),
-          });
         });
-      return admin
-        .firestore()
-        .collection("drivers")
-        .get()
-        .then(driverquerySnapshot => {
-          let messages = [];
-          driverquerySnapshot.forEach(doc => {
-            let token = doc.data()["pushDevices"][0];
-            messages.push({
-              to: token,
-              sound: "default",
-              title: data.title,
-              body: data.message,
-              channelId: "ads",
-              priority: "high",
-              data: {
-                id: 0,
-              },
-            });
-          });
-          console.log("mensajes", messages);
-          fetch("https://exp.host/--/api/v2/push/send", {
-            method: "POST",
-            mode: "no-cors",
-            headers: {
-              Accept: "application/json",
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*",
-            },
-            body: JSON.stringify(messages),
-          });
-        });
-    }
-  });
+  }
+});
