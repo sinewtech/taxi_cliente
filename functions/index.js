70c80ce77d4ec3f8631e9bba13423c21a2bb2ae8
Carlos Velasquez <imadogsi64@gmail.com>
2019-06-02 21:47:37 -0600

    Icono de notificación

--- functions/index.js
+++ functions/index.js
@@ -131,7 +131,7 @@ exports.confirm_quote = functions.database.ref("quotes/{uid}").onUpdate(snapshot
             to: token,
             sound: "default",
             title: "Carrera confirmada",
-            body: "El cliente ha aceptado el precio. Por favor comienza tu desplazamiento.",
+            body: "El cliente ha aceptado el precio propuesto.",
             data: {
               id: 3,
             },
