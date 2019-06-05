2be2cdfacb0d967ebd06eb3be0efeae43444462b
Carlos Velasquez <imadogsi64@gmail.com>
2019-06-04 19:53:35 -0600

    Ahora libre de funciones

--- functions/prueba.js
+++
@@ -1,9 +0,0 @@
-fetch("https://us-central1-taxiapp-sinewave.cloudfunctions.net/location/", {
-  method: "post",
-  body: { lat: 100, lng: 100 },
-})
-  .then(res => res.json())
-  .then(Response => {
-    console.log("success:", JSON.stringify(Response));
-  })
-  .catch(error => console.error("Error:", error));
