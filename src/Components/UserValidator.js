2341052f9ab3c64f87c9a75440ffa84c01bbf589
David Mendoza <dmendoza99@unitec.ede>
2019-07-03 16:18:13 -0600

    🔨 refactor de validacion

--- src/Components/UserValidator.js
+++ src/Components/UserValidator.js
@@ -8,29 +8,66 @@ class UserValidator extends Component {
   componentDidMount() {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
-        firebase
-          .firestore()
-          .collection("clients")
-          .doc(user.uid)
-          .get()
-          .then(snap => {
-            if (snap.exists) {
-              console.log("Usuario encontrado, pasando a Home.");
-              this.props.navigation.navigate("App");
-            } else {
-              Alert.alert(
-                "Usuario no encontrado",
-                "No hemos encontrado este usuario en el sistema. Por favor intenta de nuevo.",
-                [
-                  {
-                    text: "Ok",
-                  },
-                ]
-              );
-              firebase.auth().signOut();
-              this.props.navigation.navigate("Auth");
-            }
-          });
+        if (user.emailVerified === true) {
+          firebase
+            .firestore()
+            .collection("clients")
+            .doc(user.uid)
+            .get()
+            .then(snap => {
+              if (snap.exists) {
+                console.log("Usuario encontrado, pasando a Home.");
+                this.props.navigation.navigate("App");
+              } else {
+                Alert.alert(
+                  "Usuario no encontrado",
+                  "No hemos encontrado este usuario en el sistema. Por favor intenta de nuevo.",
+                  [
+                    {
+                      text: "Ok",
+                    },
+                  ]
+                );
+                firebase.auth().signOut();
+                this.props.navigation.navigate("Auth");
+              }
+            });
+        }
+        if (
+          user.emailVerified === false &&
+          new Date(user.metadata.creationTime).getTime() + 5 * 60 < new Date().getTime()
+        ) {
+          console.log("wenas");
+          console.log("no esta verificado");
+          Alert.alert(
+            "Confirmacion",
+            "Por favor verique su correo.",
+            [
+              {
+                text: "OK",
+                onPress: () => {
+                  firebase
+                    .auth()
+                    .currentUser.sendEmailVerification()
+                    .then(value => {
+                      firebase.auth().signOut();
+                      console.log(value);
+                    });
+                  this.props.navigation.navigate("Auth");
+                },
+              },
+              {
+                text: "Cancel",
+                onPress: () => {
+                  this.props.navigation.navigate("Auth");
+                },
+              },
+            ],
+            { cancelable: false }
+          );
+        } else {
+          this.props.navigation.navigate("App");
+        }
       } else {
         this.props.navigation.navigate("Auth");
       }
